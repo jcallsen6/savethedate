ba54cee169bc7d83b96088f1aead4cfb0b985827
Josh Callsen <jcallsen@vt.edu>
2023-02-05 18:47:34 -0800

    Converted to angular animations and added css from josh

--- src/app/message/message.component.ts
+++ src/app/message/message.component.ts
@@ -1,21 +1,62 @@
 import { Component } from '@angular/core';
-
-import { bounceInDownOnEnterAnimation, bounceOutDownOnLeaveAnimation } from 'angular-animations';
+import { trigger, transition, animate, state, style } from '@angular/animations';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-message',
   templateUrl: './message.component.html',
   styleUrls: ['./message.component.scss'],
   animations: [
-    bounceInDownOnEnterAnimation({duration: 6000, delay: 0}),
-    bounceOutDownOnLeaveAnimation({duration: 4000, delay: 0})
+    trigger('envelope', [
+      state('closed', style({
+        opacity: 1,
+        visibility: 'visible',
+        transition: 'all 2s ease'
+      })),
+      state('open', style({
+        opacity: 0,
+        transform: 'translateY(15em)'
+      })),
+      transition('open => closed', [
+        animate('1.5s')
+      ])
+    ]),
+    trigger('envelopeTop', [
+      state('closed', style({
+        opacity: 1,
+        transition: 'rotateX(180deg)'
+      })),
+      state('open', style({
+        transform: 'rotateX(0deg)'
+      })),
+      transition('open => closed', [
+        animate('1.5s')
+      ])
+    ]),
+    trigger('card', [
+      state('closed', style({
+        scale: '40%',
+      })),
+      state('open', style({
+        scale: '70%',
+        marginTop: "0",
+      })),
+      transition('open => closed', [
+        animate('1.5s')
+      ])
+    ])
   ]
 })
 export class MessageComponent {
 
-  showCard: boolean = true;
-
-  removeCard() {
-    this.showCard = false;
+  openLetter: boolean = false;
+  moveLetter: boolean = false;
+  // TODO confetti
+  // TODO fix top card not moving with body
+  // TODO fix firefox
+  // TODO fix mobile
+  openCard() {
+    this.openLetter = true;
+    timer(1500).subscribe(_ => this.moveLetter = true);
   }
 }
